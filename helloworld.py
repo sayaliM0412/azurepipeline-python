1422cbbdf5a0c32a99ef6eae24b05b299dc9716d
Sayali Joshi <sayali.joshi@magicmemories.com>
2021-01-11 23:10:01 +0000

    setup

---
+++ helloworld.py
@@ -0,0 +1,11 @@
+# Create the class
+class MyClass:
+
+    def MyFunc(self):
+        return "Hello World"
+
+# Create an object from the class
+o = MyClass()
+
+# Now we can work with the object
+print(o.MyFunc())
