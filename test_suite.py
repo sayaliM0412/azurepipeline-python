30ce947ed7a58fb111dd624df74bff711a2060a0
Sayali Joshi <sayali.joshi@magicmemories.com>
2021-01-14 22:42:52 +0000

    add something

--- test_suite.py
+++ test_suite.py
@@ -1,9 +1,7 @@
 import unittest
 import helloworld
-import summation
 
 def suite():
     t1 = unittest.TestLoader().loadTestsFromTestCase(helloworld)
-    t2 = unittest.TestLoader().loadTestsFromTestCase(summation)
-    test_suite = unittest.TestSuite([t1,t2])
+    test_suite = unittest.TestSuite([t1])
     unittest.TextTestRunner(verbosity=2).run(test_suite)
